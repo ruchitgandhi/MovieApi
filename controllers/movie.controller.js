704cd221f1a9d1db39720c4b0a68f1bed6f8cf7f
ruchitgandhi <gemini.ruchit@gmail.com>
2018-11-14 13:26:07 -0800

    Cleaned up code

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,6 +1,5 @@
 const Show = require('../models/movie.model');
 
-//Simple version, without validation or sanitation
 exports.getMovieById = function (req, res) {
     Show.find({imdbID:req.params.id, Type:"movie"})
     .then(notes => {
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
 const app = express();
+
+//Connect to MongoDB Database
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + '@cluster0-g5078.mongodb.net/movieDB?retryWrites=true', {useNewUrlParser : true});
+//mongoose.connect('mongodb+srv://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + '@cluster0-g5078.mongodb.net/movieDB?retryWrites=true', {useNewUrlParser : true});
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser : true});
+
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error while connecting to MongoDB Database. Please check your connection.\n'));
@@ -20,9 +24,11 @@ const search = require('./routes/search.route');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//API Endpoints
 app.use('/movie', movie);
 app.use('/show', show);
 app.use('/search', search);
+
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -30,11 +36,12 @@ app.listen(port, () => {
 });
 
 
+//Code to Populate Data into MongoDB
+
 //const http = require('http');
 //const Show = require('./models/movie.model');
 
-//GET DATA
-//love, batman, flash, office
+//keywords used = love, batman, flash, office
 //const urldata = {
 //  host: 'omdbapi.com',
 //  port: '80',
