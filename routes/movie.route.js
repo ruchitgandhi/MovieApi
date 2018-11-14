9c6758ca1343836ae71c8d9370aed729f5473ccf
ruchitgandhi <gemini.ruchit@gmail.com>
2018-11-14 13:24:23 -0800

    Changed search endpoint

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Require the controllers WHICH WE DID NOT CREATE YET!!
 const movie_controller = require('../controllers/movie.controller');
 
-
-// a simple test url to check that all of our files are communicating correctly.
 router.get('/:id', movie_controller.getMovieById);
 module.exports = router;
